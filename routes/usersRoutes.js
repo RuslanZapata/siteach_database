ae027c90c8b338cb46a1cda25dffe4b3ad6b97f7
RuslanZapata <ruslanzapata@gmail.com>
2021-11-27 03:54:03 -0500

    config database prod

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -9,5 +9,5 @@ module.exports = (app) => {
     app.post('/api/users/create', UsersController.register)
     app.post('/api/users/login', UsersController.login)
     app.post('/api/session/create', SesionController.register)
-    app.put('/api/session/update', SesionController.actualizar)
+    app.post('/api/session/update', SesionController.actualizar)
 }
